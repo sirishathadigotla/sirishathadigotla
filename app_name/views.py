cfaac2cdcff87f55592e8ea343cfa1df5a4fb631
Thadigotla sreekanth Reddy <sreekanth@ampcome.com>
2023-11-09 10:59:11 +0530

    first commit

---
+++ app_name/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def silk(request):
+    return HttpResponse('we are not talking about dairy milk silk')
+
+
+
+
+
+
+
+
+
+
+
+
+
